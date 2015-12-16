074cbc6d839cd2c2f5460937ffda42ff00138225
gg19930416 <843968901@qq.com>
2015-12-16 13:25:19 +0800

    Initial Commit

---
+++ adaptiveLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adaptiveLabel
+//
+//  Created by 郭刚 on 15/12/16.
+//  Copyright (c) 2015年 郭刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ adaptiveLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  adaptiveLabel
+//
+//  Created by 郭刚 on 15/12/16.
+//  Copyright (c) 2015年 郭刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
